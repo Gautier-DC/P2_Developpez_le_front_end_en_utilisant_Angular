e3017611070b57c26e6e5a94a3cf91fef95ca56b
gautier <gautier@MacBook-Pro.local>
2025-06-19 17:12:40 +0200

    add types and header

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,19 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { CountryOlympicData } from '../models/Olympic';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympics$ = new BehaviorSubject<
+    CountryOlympicData[] | null | undefined
+  >(undefined);
 
   constructor(private http: HttpClient) {}
 
   loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
+    return this.http.get<CountryOlympicData[]>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
       catchError((error, caught) => {
         // TODO: improve error handling
@@ -25,7 +28,21 @@ export class OlympicService {
     );
   }
 
-  getOlympics() {
+  getOlympics(): Observable<CountryOlympicData[] | null | undefined> {
     return this.olympics$.asObservable();
+  }
+
+  getMedalsByCountry(): { name: string; totalMedals: number }[] {
+    const data = this.olympics$.value;
+
+    if (!data) return [];
+
+    return data.map((country: CountryOlympicData) => ({
+      name: country.country,
+      totalMedals: country.participations.reduce(
+        (sum: number, p: any) => sum + p.medalsCount,
+        0
+      ),
+    }));
   }
 }
---
+++ src/components/medals-pie-chart/medals-pie-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medals-pie-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './medals-pie-chart.component.html',
+  styleUrl: './medals-pie-chart.component.scss'
+})
+export class MedalsPieChartComponent {
+
+}
